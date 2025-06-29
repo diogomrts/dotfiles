2247852662eac8bfebb97a71b34ba66a3544c3ce
Diogo Martins <diogo.felix.martins@gmail.com>
2025-06-29 20:50:32 +0100

    aur packages script

--- run_once_install_aur_packages.sh
+++ run_once_install_aur_packages.sh
@@ -2,4 +2,9 @@
 
 # some packages hang when installing with ansible so install here
 
-#yay -S --needed --noconfirm ttf-ms-win10-auto ttf-ms-win11-auto
+#bruno \
+#ttf-ms-win10-auto \
+
+yay -S --needed --noconfirm \
+ttf-ms-win11-auto \
+zen-browser-bin
